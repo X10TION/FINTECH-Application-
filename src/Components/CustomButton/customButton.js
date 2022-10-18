abd4d693f0dd1b28e46ab48d1f04e7c9406ae36b
Christopher White <x0tion007@gmail.com>
2022-10-18 12:13:08 +0100

    finish signin screen

--- src/Components/CustomButton/customButton.js
+++ src/Components/CustomButton/customButton.js
@@ -1,10 +1,18 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 
-const customButton = ({ onPress,text, type='PRIMARY'}) => {
+const customButton = ({ onPress,text, type='PRIMARY', bgColor, fgColor}) => {
   return (
-    <Pressable onPress={onPress} style={[styles.container,styles[`container_${type}`]]}>
-      <Text style={[, styles[`text_${type}`]]}>{text}</Text>
+    <Pressable onPress={onPress}
+     style={[styles.container,
+     styles[`container_${type}`],
+    bgColor ? {backgroundColor: bgColor} : {},
+     ]}>
+      <Text style={[styles.text,
+         styles[`text_${type}`],
+         fgColor ? {color: fgColor} : {}, 
+         
+         ]}>{text}</Text>
     </Pressable>
   )
 }
@@ -15,7 +23,7 @@ const styles = StyleSheet.create({
        
         width:'100%',
         padding:15,
-        marginVertical:5,
+        marginVertical:25,
         alignItems: 'center',
         borderRadius:5,
 
--- src/Screens/SigninScreen.js/Signin.js
+++ src/Screens/SigninScreen.js/Signin.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native'
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native'
 import logo from '../../../assets/images/sign.gif'
 import CustomInput from '../../Components/customInput/customInput'
 import CustomButton from '../../Components/CustomButton'
@@ -10,12 +10,27 @@ const onSignInPressed = () => {
 const onForgotPassword = () => {
   console.log("forgrt password")
 }
+const onFacebook = () => {
+  console.log('facebook')
+}
+const onGoogle = () => {
+  console.log('google')
+}
+
+const onApple= () => {
+  console.log('Apple')
+}
+const onSignup = () => {
+  console.log('sign up')
+}
+
 
 const Signin = () => {
     const {username, setUsername} = useState('')
     const {password, setPassword} = useState('')
     const {height} = useWindowDimensions()
   return (
+    <ScrollView showsVerticalScrollIndicator={false}>
     <View style={styles.divin}>
      
       <Image source={logo} style={[styles.logo,{height: height * 0.3}]} resizeMode={'contain'}/>
@@ -29,11 +44,48 @@ const Signin = () => {
        setValue={setPassword}
        secureTextEntry
        />
-       <CustomButton text="Sign In" onPress={onSignInPressed}/>
-       <CustomButton text="Forgot Password?" onPress={onForgotPassword} type='TERTIARY'/>
+       <CustomButton
+        text="Sign In"
+         onPress={onSignInPressed}/>
+       <CustomButton 
+       text="Forgot Password?" 
+       onPress={onForgotPassword} 
+       type='TERTIARY'/>
+
+       <CustomButton 
+       text="Sign up with Facebook"
+        onPress={onFacebook} 
+        type='TERTIARY'
+        bgColor={"#e7eafa"}
+        fgColor={"#4765a9"}
+        />
+       <CustomButton
+        text="Sign up with Google"
+        onPress={onGoogle} type='TERTIARY'
+        bgColor={"#fae9ea"}
+        fgColor={"#dd4d44"}
+        />
+       <CustomButton 
+       text="Sign up with Apple" 
+       onPress={onApple} 
+       type='TERTIARY'
+       bgColor={"#e3e3e3"}
+      fgColor={"#363636"}
+        />
+           <CustomButton 
+       text="Don't have an account? create one" 
+       onPress={onSignup} 
+       type='TERTIARY'
+       
+        />
     </View>
+    </ScrollView>
   )
 }
+
+
+
+
 
 
 const styles = StyleSheet.create({
